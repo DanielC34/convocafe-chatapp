4fd659bf8810b67b33d66084aeb2326ccfa50067
Daniel Chanda <danielchanda41@gmail.com>
2025-04-03 21:39:03 +0200

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="font-bold flex mt-8 ml-5 bg-blue-300">Hello React</h1>
+    </div>
+  )
+}
+
+export default App
